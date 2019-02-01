6d94d3052604840daf73c0ea78bdc63091e0f644
laurdawn <652751663@qq.com>
2019-02-01 15:39:53 +0800

    test

---
+++ src/main/java/com/laurdawn/website/config/RabbitMQConfig.java
@@ -0,0 +1,12 @@
+package com.laurdawn.website.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/** 
+* @author  laurdawn 
+* @version 2019年2月1日 下午2:22:26 
+*/
+@Configuration
+public class RabbitMQConfig {
+
+}
